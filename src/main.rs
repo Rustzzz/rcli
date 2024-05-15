c56b9e1c0e391574eb6eb78f99a9b10b58a59457
kkthmyh <kkth.xj@gmail.com>
2024-05-15 20:25:40 +0800

    feat: 支持输出格式yaml或者json

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use rcli::{process_csv, Opts, Subcommand};
 fn main() -> Result<()> {
     let cli = Opts::parse();
     match cli.cmd {
-        Subcommand::Csv(csv_opt) => process_csv(&csv_opt.input, &csv_opt.output),
+        Subcommand::Csv(csv_opt) => {
+            let output = if let Some(output) =csv_opt.output {
+                output.clone()
+            } else {
+               format!("output.{}",csv_opt.format)
+            };
+            process_csv(&csv_opt.input, &output, csv_opt.format)
+        }
     }
 }
--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::path::Path;
+use core::fmt;
+use std::{path::Path, str::FromStr};
 
 #[derive(Debug, Parser)]
 #[command(name = "rcli")]
@@ -14,12 +15,23 @@ pub enum Subcommand {
     Csv(CsvOpt),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+    Toml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpt {
     #[arg(short, long,value_parser = verify_file_exists)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
+    // default_value 会做一次自动转换 from &str to String
+    #[arg(short, long)]
+    pub output: Option<String>,
+    // default_value_t 不会自动转换，需要和字段类型完全对应
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
@@ -31,5 +43,38 @@ fn verify_file_exists(file: &str) -> Result<String, &'static str> {
         Ok(file.into())
     } else {
         Err("File not exist!")
+    }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(format: &str) -> Result<Self, Self::Err> {
+        let format = match format {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(anyhow::anyhow!("Output format doesnt exist")),
+        };
+        format
+    }
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(value: OutputFormat) -> Self {
+        match value {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+            OutputFormat::Toml => "toml",
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&'static str>::into(*self))
     }
 }

5672e5da4abb59595e7359f4c532a6a019ffa5f8
kkthmyh <kkth.xj@gmail.com>
2024-05-21 23:06:25 +0800

    feat: serve dir

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,24 +1,38 @@
 use anyhow::Result;
 use axum::{
     extract::{Path, State},
-    http::StatusCode,
+    response::Html,
     routing::get,
     Router,
 };
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
-use tokio::{fs, net::TcpListener};
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 use tracing::info;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct HttpServerState {
     path: PathBuf,
+}
+
+enum PathType {
+    NotFound,
+    IsDir,
+    IsFile,
 }
 
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on port {}", path, addr);
-    let state = HttpServerState { path };
+    let state = HttpServerState { path: path.clone() };
+    let dir_service = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_gzip()
+        .precompressed_zstd();
     let router = Router::new()
+        .nest_service("/tower", dir_service)
         .route("/*path", get(file_handler))
         .with_state(Arc::new(state));
     let listener = TcpListener::bind(addr).await?;
@@ -29,16 +43,54 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
 async fn file_handler(
     State(state): State<Arc<HttpServerState>>,
     Path(path): Path<String>,
-) -> (StatusCode, String) {
+) -> Html<String> {
     let p = std::path::Path::new(&state.path).join(path);
     info!("{:?}", p);
     if !p.exists() {
-        return (
-            StatusCode::NOT_FOUND,
-            format!("File {} not found", p.display()),
-        );
+        return gen_resp(p, PathType::NotFound).await;
     } else {
-        let content = fs::read_to_string(p).await.unwrap();
-        (StatusCode::OK, format!("{}", content))
+        if p.is_dir() {
+            return gen_resp(p, PathType::IsDir).await;
+        } else {
+            return gen_resp(p, PathType::IsFile).await;
+        }
     }
+}
+
+async fn gen_resp(path: PathBuf, status: PathType) -> Html<String> {
+    let ret = match status {
+        PathType::NotFound => "Not Found".into(),
+        PathType::IsDir => {
+            let mut files = Vec::with_capacity(128);
+            let dir = std::fs::read_dir(path).unwrap();
+            for item in dir {
+                let item = item.unwrap().path();
+                files.push(item);
+            }
+            let mut li_list = String::new();
+            for file in files {
+                let li = format!("<li>{}</li>", file.to_str().unwrap());
+                li_list.push_str(li.as_str())
+            }
+            let ul = format!("<ul>{}</ul>", li_list);
+            ul
+        }
+        PathType::IsFile => tokio::fs::read_to_string(path).await.unwrap(),
+    };
+
+    let html = format!(
+        r#"<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Hello World!</title>
+            </head>
+            <body>
+            {}
+            </body>
+        </html>"#,
+        ret
+    );
+    Html(html)
 }
